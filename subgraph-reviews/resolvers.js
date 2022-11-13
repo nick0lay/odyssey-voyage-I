dbe7392648e8ba6fb77ab86927cc957390465a32
Nikolay Nikiforchuk <nikolay.nikiforchuk@coople.com>
2022-11-13 14:27:32 +0100

    Federated graph implemented.

--- subgraph-reviews/resolvers.js
+++ subgraph-reviews/resolvers.js
@@ -4,6 +4,19 @@ const resolvers = {
       return dataSources.reviewsAPI.getLatestReviews();
     }
   },
+  Review: {
+    location: ({locationId}) => {
+      return {id: locationId}
+    }
+  },
+  Location: {
+    overallRating: ({id}, _, {dataSources}) => {
+      return dataSources.reviewsAPI.getOverallRatingForLocation(id);
+    },
+    reviewsForLocation: ({id}, _, {dataSources}) => {
+      return dataSources.reviewsAPI.getReviewsForLocation(id);
+    },
+  },
   Mutation: {
     submitReview: (_, {locationReview}, {dataSources}) => {
       const newReview = dataSources.reviewsAPI.submitReviewForLocation(locationReview);
